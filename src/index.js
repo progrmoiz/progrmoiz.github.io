3d7fd7f4344a25bed3af87f39566c1d08e29d8c6
Abdul Moiz <progrmoiz@gmail.com>
2019-03-08 11:43:02 +0500

    added small images and lazyload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@
 require('normalize.css/normalize.css');
 require('./styles/index.scss');
 require('./scripts/zoom-vanilla.min');
+import LazyLoad from "vanilla-lazyload";
 
 document.addEventListener("DOMContentLoaded", () => {
 
 });
+
+const myLazyLoad = new LazyLoad();
+// After your content has changed...
+myLazyLoad.update();
